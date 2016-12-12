540a4163c2d4db515e450fa0904d8c87f139a54a
Chiou Chu <enjoychu@gmail.com>
2016-12-12 18:51:12 +0800

    Use filter to disable buttons

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 
 class Filter extends Component {
     render() {
-        const filterActions = this.props.filterActions;
+        const { filter, filterActions } = this.props;
         return (
             <div>
-                <button onClick={() => filterActions.setFilter('SHOW_ALL')}>All</button>
-                <button onClick={() => filterActions.setFilter('SHOW_COMPLETED')}>Completed</button>
-                <button onClick={() => filterActions.setFilter('SHOW_UNCOMPLETED')}>Uncompleted</button>
+                <button
+                  onClick={() => filterActions.setFilter('SHOW_ALL')}
+                  disabled={ filter === 'SHOW_ALL' }
+                >All</button>
+                <button
+                  onClick={() => filterActions.setFilter('SHOW_COMPLETED')}
+                  disabled={ filter === 'SHOW_COMPLETED' }
+                >Completed</button>
+                <button
+                  onClick={() => filterActions.setFilter('SHOW_UNCOMPLETED')}
+                  disabled={ filter === 'SHOW_UNCOMPLETED' }
+                >Uncompleted</button>
             </div>
         );
     }
