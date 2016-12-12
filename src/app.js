1c7182c8f11da7f9890c20da9d4305dc6751042f
Chiou Chu <enjoychu@gmail.com>
2016-12-12 16:25:39 +0800

    Set todos data into app

--- src/app.js
+++ src/app.js
@@ -2,13 +2,36 @@ import React, { Component } from 'react';
 import TodoList from './components/TodoList';
 import TodoAdd from './components/TodoAdd';
 
+const todos = [
+    {
+        task: 'Install packages',
+        isCompleted: false
+    },
+    {
+        task: 'Add webpack.config.js',
+        isCompleted: false
+    },
+    {
+        task: 'Break UI into components',
+        isCompleted: false
+    }
+];
+
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            todos
+        };
+    }
+
     render() {
         return (
             <div>
                 <h1>React Todo List</h1>
                 <TodoAdd />
-                <TodoList />
+                <TodoList todos={this.state.todos} />
             </div>
         );
     }
