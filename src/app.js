a884f72db059772cdb526ab230f990fe4f2f51cb
Chiou Chu <enjoychu@gmail.com>
2016-12-12 18:35:52 +0800

    Use connect() function and remove unneeded code

--- src/app.js
+++ src/app.js
@@ -1,80 +1,47 @@
 import React, { Component } from 'react';
-import TodoList from './components/TodoList';
-import TodoAdd from './components/TodoAdd';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import classNames from 'classnames';
 
+import * as TodosActions from './actions/todos';
+import TodoList from './components/TodoList';
+import TodoAdd from './components/TodoAdd';
 import '../css/style.css';
 
-const todos = [
-    {
-        task: 'Install packages',
-        isCompleted: false
-    },
-    {
-        task: 'Add webpack.config.js',
-        isCompleted: false
-    },
-    {
-        task: 'Break UI into components',
-        isCompleted: false
-    }
-];
-
 class App extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            todos
-        };
-        this._saveTask = this._saveTask.bind(this);
-        this._addTask = this._addTask.bind(this);
-        this._deleteTask = this._deleteTask.bind(this);
-        this._completeTask = this._completeTask.bind(this);
-    }
-
     render() {
+        const { todos, todosActions } = this.props;
         return (
             <div>
                 <h1 className={classNames('title')}>React Todo List</h1>
-                <TodoAdd addTask={this._addTask} />
+                <TodoAdd addTask={todosActions.addTask} />
                 <TodoList
-                    todos={this.state.todos}
-                    saveTask={this._saveTask}
-                    deleteTask={this._deleteTask}
-                    completeTask={this._completeTask}
+                    todos={todos}
+                    saveTask={todosActions.editTask}
+                    deleteTask={todosActions.deleteTask}
+                    completeTask={todosActions.toggleTask}
                 />
             </div>
         );
     }
-
-    _saveTask(idx, val) {
-        let newTodos = [...this.state.todos];
-        newTodos[idx] = Object.assign({}, newTodos[idx], { task: val });
-        this.setState({ todos: newTodos });
-    }
-
-    _addTask(val) {
-        let newTodos = [...this.state.todos];
-        newTodos.push(
-            {
-                task: val,
-                isCompleted: false
-            });
-        this.setState({ todos: newTodos });
-    }
-
-    _deleteTask(idx) {
-        let newTodos = [...this.state.todos];
-        newTodos.splice(idx, 1);
-        this.setState({ todos: newTodos });
-    }
-
-    _completeTask(idx) {
-        let newTodos = [...this.state.todos];
-        newTodos[idx] = Object.assign({}, newTodos[idx], { isCompleted: !newTodos[idx].isCompleted });
-        this.setState({ todos: newTodos });
-    }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+    return {
+        todos: state.todos,
+        filter: state.filter
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        todosActions: bindActionCreators(TodosActions, dispatch)
+    };
+};
+
+const TodoAppContainer = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(App);
+
+export default TodoAppContainer;
