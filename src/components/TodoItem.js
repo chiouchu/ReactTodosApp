0bda5be5721138e3e01fb85db27ec471835ac2cd
Chiou Chu <enjoychu@gmail.com>
2016-12-12 16:31:30 +0800

    Use isEditing state in TodoItem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 
 class TodoItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isEditing: false
+        };
+    }
+
     render() {
-        const todo = this.props.todo;
+        const todo = this.props;
+
+        if (this.state.isEditing) {
+            return (
+                <tr>
+                    <th><input type="text" defaultValue={task} /></th>
+                    <th>
+                        <button>Save</button>
+                        <button>Cancel</button>
+                    </th>
+                </tr>
+            );
+        }
+
         return (
             <tr>
                 <th>{todo.task}</th>
