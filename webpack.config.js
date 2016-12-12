4ce44d04b9116619dc0e8b8047106b3170c0a12e
Chiou Chu <enjoychu@gmail.com>
2016-12-12 16:12:42 +0800

    Webpack init

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: [
+        'webpack-hot-middleware/client?reload=true',
+        './src/index.js'
+    ],
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.[hash].js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+    resolve: {
+        extensions: ['', '.js']
+    }
+};
