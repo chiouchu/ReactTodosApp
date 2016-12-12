d43cddde0d7bd49d14579c1b2d4f7049f7acc986
Chiou Chu <enjoychu@gmail.com>
2016-12-12 18:36:55 +0800

    Remove unneeded code

--- src/index.js
+++ src/index.js
@@ -3,17 +3,9 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 
 import todoApp from './reducers';
-import * as todoActions from './actions/todos';
 import App from './app';
 
 let store = createStore(todoApp);
-console.log(store.getState());
-
-let listener = store.subscribe(() =>
-    console.log(store.getState())
-);
-store.dispatch(todoActions.addTask('Test'));
-
 
 ReactDOM.render(
     <App />,
