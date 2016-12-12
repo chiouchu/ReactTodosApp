d5178f08b3c36f0b87d66a885c3395868d50c218
Chiou Chu <enjoychu@gmail.com>
2016-12-12 18:09:13 +0800

    Setting store and tracing store method

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
+import todoApp from './reducers';
+import * as todoActions from './actions/todos';
 import App from './app';
+
+let store = createStore(todoApp);
+console.log(store.getState());
+
+let listener = store.subscribe(() =>
+    console.log(store.getState())
+);
+store.dispatch(todoActions.addTask('Test'));
+
 
 ReactDOM.render(
     <App />,
