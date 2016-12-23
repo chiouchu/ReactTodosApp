de50988ed8b2f468da22d3e1510cfe9bb28de2ad
Chiou Chu <enjoychu@gmail.com>
2016-12-23 17:33:58 +0800

    Filter action testing

---
+++ test/filter.test.js
@@ -0,0 +1,16 @@
+import { expect } from 'chai';
+import * as types from '../src/constants/ActionTypes';
+import * as filter from '../src/actions/filter';
+
+describe('filter action testing', () => {
+    it('should create an action to set filter', () => {
+        expect(
+            filter.setFilter('SHOW_ALL')
+        ).to.be.deep.equal(
+            {
+                type: types.SET_FILTER,
+                filter: 'SHOW_ALL'
+            }
+        );
+    });
+});
